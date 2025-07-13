5ebea6e2aa3e247765e26210803678eb88a2a8b2
vini <vini.ba.dev@gmail.com>
2025-07-13 19:37:22 -0300

    init

---
+++ simplificado/src/main/java/com/picpay/simplificado/SimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.simplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplificadoApplication.class, args);
+	}
+
+}
