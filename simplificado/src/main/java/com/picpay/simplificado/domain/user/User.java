a77233e6cfb1032bc40fbfe259ebea8090d3655c
vini <vini.ba.dev@gmail.com>
2025-07-14 11:15:51 -0300

    fix: corrige a coluna email na entidade, forçando-a ser única

--- simplificado/src/main/java/com/picpay/simplificado/domain/user/User.java
+++ simplificado/src/main/java/com/picpay/simplificado/domain/user/User.java
@@ -22,6 +22,7 @@ public class User {
     private String lastName;
     @Column (unique = true)
     private String cpf;
+    @Column (unique = true)
     private String email;
     private String password;
     private BigDecimal balance;
