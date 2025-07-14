50bcc78bc40e29972274a7e193391b16c318c965
vini <vini.ba.dev@gmail.com>
2025-07-14 17:45:42 -0300

    add: adiciona o serviço de notificação

---
+++ simplificado/src/main/java/com/picpay/simplificado/DTO/NotificationDTO.java
@@ -0,0 +1,4 @@
+package com.picpay.simplificado.DTO;
+
+public record NotificationDTO(String status, String message) {
+}
---
+++ simplificado/src/main/java/com/picpay/simplificado/domain/NotificationQueue.java
@@ -0,0 +1,4 @@
+package com.picpay.simplificado.domain;
+
+public record NotificationQueue(Long id, String message) {
+}
